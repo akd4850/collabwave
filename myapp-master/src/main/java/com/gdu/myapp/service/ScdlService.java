fe84b16e970b369f5bd28acbb0121bc188d10959
shyeom <s.h.yeom94@gmail.com>
2024-06-14 12:06:24 +0900

    [염상훈] 일정 저장 시 전사일정 / 개인일정 색깔로 구분

--- myapp-master/src/main/java/com/gdu/myapp/service/ScdlService.java
+++ myapp-master/src/main/java/com/gdu/myapp/service/ScdlService.java
@@ -7,12 +7,12 @@ import com.gdu.myapp.dto.ScdlDto;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface ScdlService {
-	// 일정 등록 INSERT
+	// INSERT 일정 등록 
 	int registerScheduler(HttpServletRequest request);  
-	// 일정 목록 불러오기 SELECT 
+	// SELECT 일정 목록 불러오기  
 	List<ScdlDto> getScheduleList(HttpServletRequest request);
-	// 일정 수정하기
+	// UPDATE 일정 수정하기
 	int updateSchedule(ScdlDto scdl);
-	// 일정 삭제하기 DELETE 
+	// DELETE 일정 삭제하기  
 	int deleteSchedule(int scdlNo);
 }
